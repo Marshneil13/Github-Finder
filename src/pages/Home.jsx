2389b47b6904682d7b94b0db14da76fdada73882
marshnil_13 <btech10225.20@bitmesra.ac.in>
2022-03-18 23:43:13 +0530

    API setup 1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import UserResults from "../components/users/UserResults"
 
 function Home() {
   return (
     <>
-        <h1 className="text-6xl">Welcome</h1>
+        {/* SEARCH COMPONENT */}
+        <UserResults/>
     </>
   )
 }
