a8ffb02dc31bf2a30c4dcdb5af070b09fefa16f7
Dhananjay <dhananjayman11@gmail.com>
2022-03-20 12:04:57 +0530

    api call without headers

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
 import Spinner from '../layout/Spinner'
 
 function UserResults() {
-  const {users, setUsers} = useState([])
+  const [users, setUsers] = useState([])
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
@@ -11,31 +11,32 @@ function UserResults() {
   }, [])
 
   const fetchUsers = async () => {
-    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-      headers: {
-        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-      },
-    })
-
+    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`)
+    // , {
+    //   headers: {
+    //     Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+    //   },
+    // }
+    console.log("res.data", response);
     const data = await response.json()
-    console.log(data);
+    console.log("response data in UserResults ", data);
 
     setUsers(data)
     setLoading(false)
   }
 
-  if(!loading){
+  if (!loading) {
     return (
       <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
         {users.map((user) => (
-          <h3>{user.login}</h3>
+          <h3 key={user.id}>{user.login}</h3>
         ))}
       </div>
     )
-  }else{
-      return <Spinner/>
+  } else {
+    return <Spinner />
   }
-    
+
 }
 
 export default UserResults
