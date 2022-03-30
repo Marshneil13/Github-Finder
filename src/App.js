06a9b6721503a1226d1b952bad181ccdda9d9366
marshnil_13 <btech10225.20@bitmesra.ac.in>
2022-03-30 18:07:05 +0530

    alert setup 1

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import NotFound from './pages/NotFound';
 import Footer from './components/layout/Footer';
 import Navbar from './components/layout/Navbar';
 import {GithubProvider} from './context/github/GithubContext'
+import {AlertProvider} from './context/alert/AlertContext'
 function App() {
   return (
     <GithubProvider>
+    <AlertProvider>
     <Router>
       <div className='flex flex-col justify-between h-screen'>
         <Navbar/>
@@ -23,6 +25,7 @@ function App() {
         <Footer/>
       </div>
     </Router>
+    </AlertProvider>
     </GithubProvider>
   );
 }
