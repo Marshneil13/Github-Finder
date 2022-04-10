cc9466473d41d7ca7648f84c6ee03a23c3017642
Dhananjay <dhananjayman11@gmail.com>
2022-04-11 00:06:33 +0530

    repo list error fix

--- src/components/repos/RepoItem.jsx
+++ src/components/repos/RepoItem.jsx
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types'
-import {FaEye, FaInfo, FaLink, FaStar, FaUtensils} from 'react-icons/fa'
+import { FaEye, FaInfo, FaLink, FaStar, FaUtensils } from 'react-icons/fa'
 
 
-function RepoItem({repo}) {
-    const {
+function RepoItem({ repo }) {
+  console.log("Repos in RepoItem", repo);
+  const {
     name,
     description,
     html_url,
@@ -14,20 +15,21 @@ function RepoItem({repo}) {
   } = repo
 
 
+
   return (
     <div className='mb-2 rounded-md card bg-base-200 hover:bg-base-300'>
       <div className='card-body'>
         <h3 className='mb-2 text-xl font-semibold'>
           <a href={html_url}>
-            <FaLink className='inline mr-1'/>{name}
+            <FaLink className='inline mr-1' />{name}
           </a>
         </h3>
-        </div>
+      </div>
     </div>
   )
 }
 
 RepoItem.propTypes = {
-    repo: PropTypes.object.isRequired
+  repo: PropTypes.object.isRequired
 }
 export default RepoItem
